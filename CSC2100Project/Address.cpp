142a2c43ac35ceca3247d0992bf8680570dda0af
Dave Lea <eclipse__16@hotmail.com>
2016-03-19 19:42:09 -0400

    address definition

--- CSC2100Project/Address.cpp
+++ CSC2100Project/Address.cpp
@@ -5,10 +5,20 @@ using namespace std;
 
 Address::Address()
 {
+	mailingStreet = "";
+	city = "";
+	state = "";
+	postalCode = "";
+	country = "";
 }
 
-Address::Address(string, string, string, string, string)
+Address::Address(string str, string c, string sta, string pc, string coun)
 {
+	mailingStreet = str;
+	city = c;
+	state = sta;
+	postalCode = pc;
+	country = coun;
 }
 
 Address::~Address()
